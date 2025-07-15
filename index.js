165150a5bf4792b54291c691fe3d7ad41203d190
amit from linux machine <amitthakur10sep@gmail.com>
2025-07-15 23:27:47 +0530

    added script

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const options = {
-    // origin: "https://job-portal-frontend-ashen-seven.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-frontend-ashen-seven.vercel.app",
+    // origin: "http://localhost:5173",
     credentials: true
 }
 
